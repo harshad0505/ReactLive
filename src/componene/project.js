7b5ee367dffa9542288d6be63688621264c1d230
Harshad Chavan <harshadchavan05@gmail.com>
2021-03-13 10:17:58 +0530

    third time

--- src/componene/project.js
+++ src/componene/project.js
@@ -7,7 +7,7 @@ function project(){
       <div className="project" id="4">
         <div className="box3" data-aos="fade-right">
           <div className="pro">
-            <img src='/Image/harshad.png'></img>
+            <img src={require('/Image/harshad.png')}></img>
             <h2>The Portfoli Website</h2>
             <p>This Wesbite Create with the help of html,css,js & also with the help of React</p>
           </div>
@@ -15,7 +15,7 @@ function project(){
 
         <div className="box3" data-aos="zoom-out">
           <div className="pro">
-            <img src='/Image/weat.png'></img>
+            <img src={require('/Image/weat.png')}></img>
             <h2>WEATHER FORCAST</h2>
             <p>This Wesbite Create with the help of html,css,js & also with the help of React</p>
           </div>
@@ -23,7 +23,7 @@ function project(){
 
         <div className="box3" data-aos="fade-left">
           <div className="pro" >
-            <img src='/Image/game.png'></img>
+            <img src={require('/Image/game.png')}></img>
             <h2>GUESS THE NUMBER</h2>
             <p>This Wesbite Create with the help of html,css,js & also with the help of React</p>
           </div>
